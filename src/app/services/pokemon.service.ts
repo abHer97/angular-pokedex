a7712eaccb444433871ad79e7224bc7b2d938b77
Abel Herrera <ahpard97@gmail.com>
2023-01-07 17:19:19 -0600

    add getPokemons function

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environment/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonService {
+  baseUrl: string = environment.baseUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPokemons(index: number) {
+    return this.http.get<any>(`${this.baseUrl}/pokemon/${index}`);
+  }
 }

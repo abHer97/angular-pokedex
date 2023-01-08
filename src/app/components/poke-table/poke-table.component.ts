c073314dce1451e68d0b5e810d84bf65f0e6615b
Abel Herrera <ahpard97@gmail.com>
2023-01-07 18:33:58 -0600

    add get and render pokemons

--- src/app/components/poke-table/poke-table.component.ts
+++ src/app/components/poke-table/poke-table.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-poke-table',
   templateUrl: './poke-table.component.html',
-  styleUrls: ['./poke-table.component.css']
+  styleUrls: ['./poke-table.component.css'],
 })
-export class PokeTableComponent {
+export class PokeTableComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'image', 'name'];
+  data: any[] = [];
+  dataSource = new MatTableDataSource<any>(this.data);
 
+  // @ts-ignore
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  pokemons = [];
+
+  constructor(private pokemonService: PokemonService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.getPokemons();
+  }
+
+  getPokemons() {
+    let pokemonData;
+
+    for (let i = 1; i <= 150; i++) {
+      this.pokemonService.getPokemons(i).subscribe(
+        (res) => {
+          pokemonData = {
+            position: i,
+            image: res.sprites.front_default,
+            name: res.name,
+          };
+
+          this.data.push(pokemonData);
+          this.dataSource = new MatTableDataSource<any>(this.data);
+          this.dataSource.paginator = this.paginator;
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  // @ts-ignore
+  getRow(row) {
+    this.router.navigateByUrl(`/pokeDetail/${row.position}`);
+  }
 }
